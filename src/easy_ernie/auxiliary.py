877eebb26e4f2cd35a70e902f03d233fa495a118
XiaoXinYo <1104361313@qq.com>
2023-12-06 22:04:37 +0800

    修复getTimestamp

--- src/easy_ernie/auxiliary.py
+++ src/easy_ernie/auxiliary.py
@@ -1,7 +1,7 @@
 import time
 
 def getTimestamp(ms: bool=False) -> int:
-    return int(time.time() * 1 if ms else 1000)
+    return int(time.time() * (1000 if ms else 1))
 
 def timeToTimestamp(text):
     timeArray = time.strptime(text, '%Y-%m-%d %H:%M:%S')
